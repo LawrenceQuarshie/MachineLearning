3316285aba39a26b3a1b507e5e5014d696546434
Lawrence <lquarshie29@gmail.com>
2024-02-07 18:50:41 -0500

    Added a .py file that demonstrates the calculates the mean of a dataset.

---
+++ a.py
@@ -0,0 +1,7 @@
+import numpy # import the numpy library
+
+speed = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86] # a set of data representing speed
+
+x = numpy.mean(speed) # calculates the mean of the speed
+
+print(x) # prints the value of the speed to the screen
