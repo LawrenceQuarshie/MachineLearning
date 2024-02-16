c52e8ee9bce12eb2a58198bda08148196ebc6f4a
Lawrence <lquarshie29@gmail.com>
2024-02-15 19:32:57 -0500

    Added a code that calculates the average of phone prices in Ghana.

---
+++ inf-0.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+price_15_pro_max = [16000.00, 14600.00, 21900.00, 19500.00, 19500.00, 22900.00, 22900.00, 24000.00, 26900.00]
+mean_15_pro_max = np.mean(price_15_pro_max)
+str1 = "The average price of Apple iPhone 15 Pro Max is " + str(mean_15_pro_max)
+
+print(str1)
