03f6189151d1b850ec7e9e84f3cba2fe61e38d82
Lawrence Quarshie <lquarshie29@gmail.com>
2024-02-27 09:12:26 -0500

    Added a .py file that also shows the calculation of standard deviation for a given set of numbers.

---
+++ g.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+speed = [86, 87, 88, 86, 87, 85, 86]
+
+x = np.std(speed) # standard deviation of the speed
+
+print(x)
+
+# The smaller standard deviation indicates the numbers/data points are all close to the mean value
