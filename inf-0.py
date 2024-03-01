2acb1a5851b4cb08e23109596521bfd84ff407c1
Lawrence Quarshie <lquarshie29@gmail.com>
2024-03-01 07:57:37 -0500

    Modified the ML code for calculating average, median, and modal price of iPhone 15 Pro Max and iPhone 15 Pro

--- inf-0.py
+++ inf-0.py
@@ -5,12 +5,17 @@ price_15_pro_max = [16000.00, 14600.00, 21900.00, 19500.00, 19500.00, 22900.00, 
 mean_15_pro_max = np.mean(price_15_pro_max)
 median_15_pro_max = np.median(price_15_pro_max)
 mode_15_pro_max = stats.mode(price_15_pro_max)
-str1 = "The average price of Apple iPhone 15 Pro Max is " + str(mean_15_pro_max) + " and " + str(median_15_pro_max) + " and " + str(mode_15_pro_max)
+print("Apple iPhone 15 Pro Max")
+print("Average price: " + str(mean_15_pro_max))
+print("Median price: " + str(median_15_pro_max))
+print("Modal price: " + str(mode_15_pro_max))
+print(" ")
 
 price_15_pro = [14500.00, 14500.00, 16900.00, 16500.00]
 mean_15_pro = np.mean(price_15_pro)
 median_15_pro = np.median(price_15_pro)
-str2 = "The average price of Apple iPhone 15 Pro is " + str(mean_15_pro) + " and " + str(median_15_pro)
-
-print(str1)
-print(str2)
+mode_15_pro = stats.mode(price_15_pro)
+print("Apple iPhone 15 Pro")
+print("Average price: " + str(mean_15_pro))
+print("Median price: " + str(median_15_pro))
+print("Modal price: " + str(mode_15_pro))
